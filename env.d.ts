6bc19363e75570a8643c815a23b4da7b72f63c22
Avantys Design <avantys@users.noreply.github.com>
2025-09-05 12:43:58 +0200

    Revert "Merge pull request #4 from avantys/codex/configure-seo,-accessibility,-performance-features"
    This reverts commit 277dfc0b45ce8007ab07b9f83e9ab6071db7159d, reversing
    changes made to 11a2d134fc2c445ffa100282ad17821e9c7890e4.

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly SITE_URL: string;
-  readonly PUBLIC_INDEXABLE: 'true' | 'false';
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
